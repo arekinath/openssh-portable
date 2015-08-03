3940b5d3e954d472b52da4aed4f8e6f3725411d2
Alex Wilson <alex.wilson@joyent.com>
2015-08-03 16:27:44 -0700

    PubKeyPlugin support
    This adds the PubKeyPlugin directive and associated code from
    SunSSH, allowing an in-process shared library to be called
    into to check public keys for authentication.

--- servconf.c
+++ servconf.c
@@ -182,6 +182,7 @@ initialize_server_options(ServerOptions *options)
 	 */
 	options->pam_service_per_authmethod = 1;
 #endif
+	options->pubkey_plugin = NULL;
 }
 
 /* Returns 1 if a string option is unset or set to "none" or 0 otherwise. */
@@ -465,6 +466,7 @@ typedef enum {
 	sAuthenticationMethods, sHostKeyAgent, sPermitUserRC,
 	sStreamLocalBindMask, sStreamLocalBindUnlink,
 	sAllowStreamLocalForwarding, sFingerprintHash,
+	sPubKeyPlugin,
 	sDeprecated, sUnsupported
 } ServerOpCodes;
 
@@ -640,6 +642,7 @@ static struct {
 	{ "streamlocalbindunlink", sStreamLocalBindUnlink, SSHCFG_ALL },
 	{ "allowstreamlocalforwarding", sAllowStreamLocalForwarding, SSHCFG_ALL },
 	{ "fingerprinthash", sFingerprintHash, SSHCFG_GLOBAL },
+	{ "pubkeyplugin", sPubKeyPlugin, SSHCFG_ALL },
 	{ NULL, sBadOption, 0 }
 };
 
@@ -1964,6 +1967,18 @@ process_server_config_line(ServerOptions *options, char *line,
                          */
 			options->pam_service_per_authmethod = 0;
 		}
+		break;
+
+	case sPubKeyPlugin:
+		/*
+		 * Can't use parse_filename, as we need to support plain
+		 * names which dlopen will find on our lib path.
+		 */
+		arg = strdelim(&cp);
+		if (!arg || *arg == '\0')
+			fatal("%s line %d: missing file name.",
+			    filename, linenum);
+		options->pubkey_plugin = xstrdup(arg);
 		break;
 
 	case sDeprecated:
