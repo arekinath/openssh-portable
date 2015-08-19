7671959a7e5020c6ba7aa45a0d028571027dd060
Alex Wilson <alex.wilson@joyent.com>
2015-08-19 11:35:32 -0700

    Re-enable DSA keys for pk auth

--- myproposal.h
+++ myproposal.h
@@ -110,11 +110,13 @@
 	"rsa-sha2-512-cert-v01@openssh.com," \
 	"rsa-sha2-256-cert-v01@openssh.com," \
 	"ssh-rsa-cert-v01@openssh.com," \
+	"ssh-dss-cert-v01@openssh.com," \
 	HOSTKEY_ECDSA_METHODS \
 	"ssh-ed25519," \
 	"rsa-sha2-512," \
 	"rsa-sha2-256," \
-	"ssh-rsa"
+	"ssh-rsa," \
+	"ssh-dss"
 
 /* the actual algorithms */
 
